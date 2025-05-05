0dcf8ffc07e4805438f5d1938f9399deab0abaa0
Sandro Reis <consultorsandro@hotmail.com>
2025-05-05 16:21:14 -0300

    Class 105

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,13 @@
 fn main() {
-    //Class 104
-    let text = String::new();
-    let candy = String::from("Kit Kat");
+    //Class 105
+    let mut name = String::from("Sandro");
+    println!("Name: {}", name);
+    name.push_str(" Oliveira Reis");
+    println!("Name: {}", name);
 
 }
+/*
+//Class 104
+    let text = String::new();
+    let candy = String::from("Kit Kat");
+*/
