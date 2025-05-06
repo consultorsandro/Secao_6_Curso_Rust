2c41f7688aa2f9c25f2501e42cc9289215fce2c0
Sandro Reis <consultorsandro@hotmail.com>
2025-05-06 09:16:07 -0300

    Class 107

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,16 @@
 fn main() {
+    //Class 107
+    let person = String::from("Sandro");
+    
+    drop(person);// person now is invalid, deleted from memory
+}
+/*
     //Class 106
     let person = String::from("Sandro");
     //person is moved to genius, person is no longer valid.
     let genius = person; 
-}
+*/
+
 /*
     //Class 105
     let mut name = String::from("Sandro");
