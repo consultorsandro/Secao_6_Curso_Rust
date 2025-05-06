bf1b28d7a118698dd4baad83dc49840ac40874db
Sandro Reis <consultorsandro@hotmail.com>
2025-05-06 10:00:15 -0300

    Class 109

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,38 @@
 fn main() {
-    //Class 108
-    let person = String::from("Sandro");
-    let genius = person.clone(); // person is copied to genius, both are valid.
-    print!("This is {}", person);
+    // Class 109
+    let my_stack_value = 2;
+    let _my_integer_reference = &my_stack_value; // my_integer_reference is a reference to my_stack_value
+
+    let my_heap_value = String::from("Sandro");
+    let _my_heap_reference = &my_heap_value;
 }
 
- /* Class 107
+/*
+Class 108
+let person = String::from("Sandro");
+let genius = person.clone(); // person is copied to genius, both are valid.
+print!("This is {}", person);
+ */
+
+/* Class 107
     let person = String::from("Sandro");
-    
+
     drop(person);// person now is invalid, deleted from memory
 */
 /*
     //Class 106
     let person = String::from("Sandro");
     //person is moved to genius, person is no longer valid.
-    let genius = person; 
+    let genius = person;
 */
 
 /*
-    //Class 105
-    let mut name = String::from("Sandro");
-    println!("Name: {}", name);
-    name.push_str(" Oliveira Reis");
-    println!("Name: {}", name);
-     */
+//Class 105
+let mut name = String::from("Sandro");
+println!("Name: {}", name);
+name.push_str(" Oliveira Reis");
+println!("Name: {}", name);
+ */
 /*
 //Class 104
     let text = String::new();
