4dad4e6b4a47ba254eca3785a90abeaadd81c4ad
Sandro Reis <consultorsandro@hotmail.com>
2025-05-06 08:54:50 -0300

    Class 106

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,16 @@
 fn main() {
+    //Class 106
+    let person = String::from("Sandro");
+    //person is moved to genius, person is no longer valid.
+    let genius = person; 
+}
+/*
     //Class 105
     let mut name = String::from("Sandro");
     println!("Name: {}", name);
     name.push_str(" Oliveira Reis");
     println!("Name: {}", name);
-
-}
+     */
 /*
 //Class 104
     let text = String::new();
